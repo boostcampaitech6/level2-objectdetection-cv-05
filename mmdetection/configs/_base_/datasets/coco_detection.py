b8aca8bfb75f728be3d91d8bd72090059f540444
jinida <qkqh9912@gmail.com>
2024-01-08 18:32:12 +0900

    feat: add dataset

--- mmdetection/configs/_base_/datasets/coco_detection.py
+++ mmdetection/configs/_base_/datasets/coco_detection.py
@@ -1,37 +1,18 @@
-_base_ = '../faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
-model = dict(
-    neck=dict(
-        type='FPN_CARAFE',
-        in_channels=[256, 512, 1024, 2048],
-        out_channels=256,
-        num_outs=5,
-        start_level=0,
-        end_level=-1,
-        norm_cfg=None,
-        act_cfg=None,
-        order=('conv', 'norm', 'act'),
-        upsample_cfg=dict(
-            type='carafe',
-            up_kernel=5,
-            up_group=1,
-            encoder_kernel=3,
-            encoder_dilation=1,
-            compressed_channels=64)))
-
+# dataset settings
+dataset_type = 'CocoDataset'
+data_root = 'data/coco/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=64),
+    dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
 ]
-
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
@@ -42,13 +23,27 @@ test_pipeline = [
             dict(type='Resize', keep_ratio=True),
             dict(type='RandomFlip'),
             dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=64),
+            dict(type='Pad', size_divisor=32),
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img']),
         ])
 ]
-
 data = dict(
-    train=dict(pipeline=train_pipeline),
-    val=dict(pipeline=test_pipeline),
-    test=dict(pipeline=test_pipeline))
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_train2017.json',
+        img_prefix=data_root + 'train2017/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        img_prefix=data_root + 'val2017/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        img_prefix=data_root + 'val2017/',
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='bbox')
---
+++ mmdetection/configs/_base_/datasets/trash_det_album.py
@@ -0,0 +1,114 @@
+dataset_type = 'CocoDataset'
+data_root = '/data/ephemeral/home/dataset'
+
+''' todo '''
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+album = [
+    dict(
+        type='OneOf',
+        transforms=[
+            dict(type='Flip',p=1.0),
+            dict(type='RandomRotate90',p=1.0)
+        ],
+        p=0.5),
+    dict(
+        type='OneOf',
+        transforms=[
+            dict(
+                type='RandomBrightnessContrast',
+                brightness_limit=(-0.1, 0.15),
+                contrast_limit=(-0.1, 0.15),
+                p=1.0),
+            dict(
+                type='HueSaturationValue', 
+                hue_shift_limit=15, 
+                sat_shift_limit=25, 
+                val_shift_limit=10, p=0.5),
+        ],
+        p=0.5),
+    dict(type='GaussNoise', var_limit=(20, 100), p=0.3),
+    dict(
+        type='OneOf',
+        transforms=[
+            dict(type='Blur', p=1.0),
+            dict(type='GaussianBlur', p=1.0),
+            dict(type='MedianBlur', blur_limit=5, p=1.0),
+            dict(type='MotionBlur', p=1.0)
+        ],
+        p=0.1),
+]
+
+# augmentation strategy originates from DETR / Sparse RCNN
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Resize',
+        img_scale=[(480, 1024), (512, 1024), (544, 1024), (576, 1024),
+                    (608, 1024), (640, 1024), (672, 1024), (704, 1024),
+                    (736, 1024), (768, 1024), (800, 1024), (1024, 1024)],
+        multiscale_mode='value',
+        keep_ratio=True),
+    dict(
+        type='Albu',
+        transforms=album,
+        bbox_params=dict(
+            type='BboxParams',
+            format='pascal_voc',
+            label_fields=['gt_labels'],
+            min_visibility=0.0,
+            filter_lost_elements=True),
+        keymap={
+            'img': 'image',
+            'gt_bboxes': 'bboxes'
+        },
+        update_pad_shape=False,
+        skip_img_without_anno=True
+        ),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1024, 1024),
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+data = dict(train=dict(pipeline=train_pipeline), 
+            val=dict(pipeline=test_pipeline),
+            test=dict(pipeline=test_pipeline),
+            samples_per_gpu=2)
+
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + '/json_folder/train_fold_1.json',
+        img_prefix=data_root,
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + '/json_folder/val_fold_1.json',
+        img_prefix=data_root,
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + '/test.json',
+        img_prefix=data_root,
+        pipeline=test_pipeline))
+
+evaluation = dict(interval=1, metric='bbox')
