86ab5f7165322183c924d3312a4660edb28bd6a0
홍SangBeom-Hahn <hsb990917@gmail.com>
2024-01-14 21:05:08 -0500

    fix: subprocessor 실시간 로그 기능 수정
    #18

--- gpu_pool/utils/subprocessor.py
+++ gpu_pool/utils/subprocessor.py
@@ -4,4 +4,12 @@ def train(command) -> sp.CompletedProcess[str]:
     return sp.run(command, capture_output=True, text=True, shell=True)
 
 def handle_setup(command) -> sp.CompletedProcess[str]:
-    return sp.run(command, capture_output=True, text=True, shell=True)
+    process = sp.Popen(command, stdout=sp.PIPE, stderr=sp.PIPE, shell=True)
+
+    while True:
+        output = process.stdout.readline().decode()
+        
+        if output == '' and process.poll() is not None:
+            return process
+        if output:
+            print(output.strip())
